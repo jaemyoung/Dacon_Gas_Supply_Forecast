897592c852e917f0b1b88778d12d77343b14a081
jaemyoung <35715977+jaemyoung@users.noreply.github.com>
2021-11-10 18:38:55 +0900

    up

---
+++ main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 10 17:35:58 2021
+
+@author: 82109
+"""
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+plt.style.use("ggplot")
+warnings.filterwarnings('ignore')
+
+total = pd.read_csv("C:/Users/82109/GitHub/Dacon_Gas_supply_forecast/Main/Data/한국가스공사_시간별 공급량_20181231.csv", encoding="CP949")
+
+
+#d_map 딕셔너리를 생성해, 구분 encoding!      ex) A -> 0, B -> 1, ...
+d_map = {}
+for i, d in enumerate(total['구분'].unique()):
+    d_map[d] = i
+total['구분'] = total['구분'].map(d_map)
+total['연월일'] = pd.to_datetime(total['연월일'])
+#연월일에서 column 추출
+total['year'] = total['연월일'].dt.year
+total['month'] = total['연월일'].dt.month
+total['day'] = total['연월일'].dt.day
+total['weekday'] = total['연월일'].dt.weekday
+
+train_years = [2013,2014,2015,2016,2017]
+val_years = [2018]
+
+train = total[total['year'].isin(train_years)]
+val = total[total['year'].isin(val_years)]
