682b90b2be6e4af430b2d96a2104ce41b51984ed
Jaemyoung_Choi <35715977+jaemyoung@users.noreply.github.com>
2021-11-12 20:37:46 +0900

    up

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import warnings
 plt.style.use("ggplot")
 warnings.filterwarnings('ignore')
 
-total = pd.read_csv("C:/Users/82109/GitHub/Dacon_Gas_supply_forecast/Main/Data/한국가스공사_시간별 공급량_20181231.csv", encoding="CP949")
-
+total = pd.read_csv("C:/Users/user/Documents/GitHub/Dacon_Gas_Supply_Forecast/Data/한국가스공사_시간별 공급량_20181231.csv", encoding="CP949")
+hol = pd.read_csv("C:/Users/user/Documents/GitHub/Dacon_Gas_Supply_Forecast/Data/한국가스공사_대전 도시가스 수요의 특수일 효과_20210914.csv", encoding="CP949")
 
 #d_map 딕셔너리를 생성해, 구분 encoding!      ex) A -> 0, B -> 1, ...
 d_map = {}
@@ -32,3 +32,5 @@ val_years = [2018]
 
 train = total[total['year'].isin(train_years)]
 val = total[total['year'].isin(val_years)]
+total['공급량'][0]*(1+hol['추정치'][18])
+df= pd.read_csv("C:/Users/user/Documents/GitHub/Dacon_NLP_Technology_Classification/data/train.csv", encoding="UTF-8")
